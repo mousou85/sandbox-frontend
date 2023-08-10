ff744416d33315d697020e9a376d65b4316b68f8
mousou85 <mousou85@gmail.com>
2023-08-10 10:58:52 +0900

    feat: typescript 변환
    - i18n 수정

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import PrimeVue from 'primevue/config';
 import {createApp} from 'vue';
 
 import App from '@/App.vue';
+import locale_ko from '@/i18n/ko.json';
 import router from '@/router';
 import {useGlobalStore} from '@/stores/global.store';
 
@@ -18,29 +19,30 @@ app.use(createPinia());
 app.use(router());
 app.use(PrimeVue, {
   ripple: true,
-  locale: {
-    dateFormat: 'yy-mm-dd',
-    dayNames: ['일요일', '월요일', '화요일', '수요일', '목요일', '금요일', '토요일'],
-    dayNamesShort: ['일', '월', '화', '수', '목', '금', '토'],
-    dayNamesMin: ['일', '월', '화', '수', '목', '금', '토'],
-    monthNames: [
-      '1월',
-      '2월',
-      '3월',
-      '4월',
-      '5월',
-      '6월',
-      '7월',
-      '8월',
-      '9월',
-      '10월',
-      '11월',
-      '12월',
-    ],
-    monthNamesShort: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12'],
-    today: '오늘',
-    weekHeader: '주',
-  },
+  locale: locale_ko.ko,
+  // locale: {
+  //   dateFormat: 'yy-mm-dd',
+  //   dayNames: ['일요일', '월요일', '화요일', '수요일', '목요일', '금요일', '토요일'],
+  //   dayNamesShort: ['일', '월', '화', '수', '목', '금', '토'],
+  //   dayNamesMin: ['일', '월', '화', '수', '목', '금', '토'],
+  //   monthNames: [
+  //     '1월',
+  //     '2월',
+  //     '3월',
+  //     '4월',
+  //     '5월',
+  //     '6월',
+  //     '7월',
+  //     '8월',
+  //     '9월',
+  //     '10월',
+  //     '11월',
+  //     '12월',
+  //   ],
+  //   monthNamesShort: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12'],
+  //   today: '오늘',
+  //   weekHeader: '주',
+  // },
 });
 
 //set store: global store
