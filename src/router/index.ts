c84d4699255bfd51bba9aa78e300132ef90262b5
mousou85 <mousou85@gmail.com>
2023-08-10 10:59:13 +0900

    feat: typescript 변환
    - route 수정

--- src/router/index.ts
+++ src/router/index.ts
@@ -5,6 +5,34 @@ import {useGlobalStore} from '@/stores';
 import IndexView from '@/views/IndexView.vue';
 
 /**
+ * 라우터 목록
+ */
+export const routeList = {
+  index: {
+    name: 'index',
+    path: '/',
+    component: IndexView,
+    meta: {needLogin: true},
+    label: 'Home',
+    topMenuIcon: 'pi pi-home',
+  },
+  login: {
+    name: 'login',
+    path: '/login',
+    component: () => import('@/views/user/LoginView.vue'),
+    label: 'Login',
+  },
+  invest: {
+    name: 'invest',
+    path: '/invest',
+    meta: {needLogin: true},
+    redirect: '/invest/history',
+    label: 'Invest',
+    topMenuIcon: 'pi pi-dollor',
+  },
+};
+
+/**
  * 라우터 정의
  */
 export default function () {
@@ -12,25 +40,20 @@ export default function () {
     history: createWebHistory(import.meta.env.BASE_URL),
     routes: [
       {
-        path: '/',
-        name: 'index',
-        component: IndexView,
-        meta: {
-          needLogin: true,
-        },
+        path: routeList.index.path,
+        name: routeList.index.name,
+        component: routeList.index.component,
+        meta: routeList.index.meta,
       },
       {
-        path: '/about',
-        name: 'about',
-        // route level code-splitting
-        // this generates a separate chunk (About.[hash].js) for this route
-        // which is lazy-loaded when the route is visited.
-        component: () => import('@/views/AboutView.vue'),
+        path: routeList.login.path,
+        name: routeList.login.name,
+        component: routeList.login.component,
       },
       {
-        path: '/login',
-        name: 'login',
-        component: () => import('@/views/user/LoginView.vue'),
+        path: routeList.invest.path,
+        redirect: routeList.invest.redirect,
+        meta: routeList.invest.meta,
       },
     ],
   });
